597e63306e41f5273191da4ced23ef56d13dced5
charu07goel <charu07goel@gmail.com>
2018-06-22 02:14:35 +0530

    Transfer Money

--- app/src/main/java/com/example/charugoel/application/UserList.java
+++ app/src/main/java/com/example/charugoel/application/UserList.java
@@ -1,15 +1,25 @@
 package com.example.charugoel.application;
 
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -20,8 +30,9 @@ import java.util.List;
 public class UserList extends Fragment {
 
     Global uname = Global.getInstance();
+    private int counter = 0;
     SQLiteDatabase sqLiteDatabase;
-    DatabaseOperations dop;
+    DatabaseOperations dop,dop1,dop2;
     Cursor CR;
     ListAdapter listAdapter;
     ListView listView;
@@ -58,6 +69,84 @@ public class UserList extends Fragment {
 
             }while (CR.moveToNext());
         }
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, final View view, int i, long l) {
+                TextView textView = (TextView) view.findViewById(R.id.row_name);
+                final TextView list_uname = (TextView) view.findViewById(R.id.row_username);
+                View mview = getActivity().getLayoutInflater().inflate(R.layout.dialog_wallet, null);
+                View view1 = getActivity().getLayoutInflater().inflate(R.layout.fragment_wallet__profile,null);
+                final EditText amount = (EditText) view.findViewById(R.id.amt);
+                final TextView amt = (TextView) view1.findViewById(R.id.amount);
+                ImageView imageView = (ImageView) mview.findViewById(R.id.coin);
+                final EditText editText = (EditText) mview.findViewById(R.id.amt);
+
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                            counter++;
+                            editText.setText(Integer.toString(counter));
+                    }
+                });
+                String name = textView.getText().toString();
+                new AlertDialog.Builder(getActivity())
+                        .setMessage("Select amount to transfer to "+ name)
+                        .setView(mview)
+                        .setPositiveButton("Transfer", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dop1 = new DatabaseOperations(getActivity());
+                                CR = dop1.fetch(dop1);
+                                CR.moveToFirst();
+                                String a = "";
+                                do {
+
+                                    if ((CR.getString(0).equals(uname.get()))) {
+                                        a = CR.getString(1);
+                                    }
+
+                                } while (CR.moveToNext());
+
+                                if (Integer.valueOf(a) != null) {
+
+                                    if (Integer.valueOf(a) >= counter) {
+                                        Integer new_amt = Integer.valueOf(a) - counter;
+                                        dop1.update(dop1, uname.get(), a, new_amt.toString());
+                                        String x = new_amt.toString();
+
+                                        String name2 = list_uname.getText().toString();
+                                        String a2 = "";
+                                        dop2 = new DatabaseOperations(getActivity());
+                                        CR = dop2.fetch(dop2);
+                                        CR.moveToFirst();
+                                        do {
+                                            if ((CR.getString(0).equals(name2))) {
+                                                a2 = CR.getString(1);
+                                            }
+
+                                        } while (CR.moveToNext());
+
+                                        Integer new_amt2 = Integer.valueOf(a2) + counter;
+                                        dop2.update(dop2, name2, a2, new_amt2.toString());
+                                        amt.setText("₹ "+x);
+                                        Toast.makeText(getActivity(), "Transfer Successful...", Toast.LENGTH_SHORT).show();
+
+                                    } else {
+                                        Toast.makeText(getActivity(), "Not Enough Money", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                counter = 0;
+                            }
+                        })
+                        .show();
+            }
+        });
 
         return view ;
     }
